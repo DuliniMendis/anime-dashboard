a81db25275917371c80d8e7f7d59f8ca1299e896
Dulini Mendis <dulini_m@hotmail.com>
2024-08-26 12:45:33 +1000

    Add initial pages with dependencies

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+export default function LoginPage() {
+  return (
+    <main>
+      <div>
+        <div>
+          <div></div>
+        </div>
+        Login form
+      </div>
+    </main>
+  );
+}
