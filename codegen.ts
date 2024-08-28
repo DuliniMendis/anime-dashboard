5515e9351ed284a767c02cc1ccd0a40b9b374eb9
Dulini Mendis <dulini_m@hotmail.com>
2024-08-28 23:14:38 +1000

    Change codegen file paths

--- codegen.ts
+++ codegen.ts
@@ -2,13 +2,13 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   schema: 'https://graphql.anilist.co',
-  documents: ['app/graphql/*.ts'],
+  documents: ['app/lib/graphql/*.ts'],
   overwrite: true,
   generates: {
-    './app/generated/types.ts': {
+    './app/lib/generated/types.ts': {
       plugins: ['typescript'],
     },
-    './app/generated/': {
+    './app/lib/generated/': {
       preset: 'near-operation-file',
       presetConfig: { extension: '.generated.tsx', baseTypesPath: 'types.ts' },
       plugins: ['typescript-operations'],
